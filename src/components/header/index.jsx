7c2e26b0a19be730a46bc32405e8a8f9833ddfef
tusharsrivastav <tusharsrivastava2003@gmail.com>
2024-10-01 15:33:36 +0530

    changing logo to Tushar1

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <div className="header-wrapper">
       <div className="logo poppins-bold">
-        <Link to="/">Tushar</Link>
+        <Link to="/">Tushar1</Link>
       </div>
 
       {user && (
