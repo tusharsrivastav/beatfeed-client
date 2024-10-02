0df5597d08d9f78abca0b3f756ef1a582db7e305
tusharsrivastav <tusharsrivastava2003@gmail.com>
2024-10-02 15:07:02 +0530

    fixing logo text to BeatFeed

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <div className="header-wrapper">
       <div className="logo poppins-bold">
-        <Link to="/">Tushar1</Link>
+        <Link to="/">BeatFeed</Link>
       </div>
 
       {user && (
